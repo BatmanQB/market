e6ac715bf132353b0a4f92c5339e8184a719a9d2
batmanqb <artqb9@gmail.com>
2019-06-14 11:01:13 +0300

    Items were added to the content page

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,17 @@ $(function(){
 
     $('.upload__wrapper input, .upload__wrapper  select').styler();
 
+    $(".js-range-slider").ionRangeSlider({
+        skin: "round",
+        type:"double",
+        min:0,
+        max: 1000,
+        from:0,
+        to:600,
+        grid: false,
+        prefix: "$"
+    });
+
     var mixer = mixitup('.portfolio__content');
 });
 
