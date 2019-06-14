db58034842068501cc492acefb033f1e348750a5
batmanqb <artqb9@gmail.com>
2019-06-14 08:56:02 +0300

    added pages:upload and catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,13 @@
 $(function(){
-    var mixer = mixitup('.portfolio__content');
-
     $('.slider-item').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         arrows: false,
         dots: true
     });
+
+    $('.upload__wrapper input, .upload__wrapper  select').styler();
+
+    var mixer = mixitup('.portfolio__content');
 });
+
