9e597c3bafd0a05e338d43129a68f3d748f07b56
batmanqb <artqb9@gmail.com>
2019-06-25 13:46:24 +0300

    mobile version

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,52 @@
 $(function(){
+
+    $('.header__menu-btn').on('click',function(){
+        $('.menu ul').slideToggle();
+    });
+
+    $('.footer__content-title').on('click', function(){
+        $(this).next('.footer__content-list, .footer__content-box').slideToggle();
+        $(this).toggleClass('active');
+    });
+
+    $('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
     $('.slider-item').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         arrows: false,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 1601,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 1025,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+              }
+            },
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+          ]
     });
 
     $('.upload__wrapper input, .upload__wrapper  select').styler();
